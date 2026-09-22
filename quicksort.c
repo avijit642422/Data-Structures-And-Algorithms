6e724738705fb94b80269cb04cc100662debf589
Avijit Patra <avijit642422@gmail.com>
2026-09-22 17:31:11 +0530

    Add files via upload

---
+++ quicksort.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int partition(int arr[],int low,int high){
+    int pivot=arr[high];
+    int i=low-1;
+    for(int j=low;j<high;j++){
+        if(arr[j]<=pivot){
+            i++;
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+        }
+    }
+    int temp=arr[i+1];
+    arr[i+1]=arr[high];
+    arr[high]=temp;
+    return (i+1);
+}
+void quicksort(int arr[],int low,int high){
+    if(low<high){
+        int pi=partition(arr,low,high);
+        quicksort(arr,low,pi-1);
+        quicksort(arr,pi+1,high);
+    }
+}
+int main(){
+    int arr[]={3,4,7,8,1,9,5,6};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    quicksort(arr,0,size-1);
+    printf("sorted array:");
+    for(int i=0;i<size;i++){
+        printf("%d ",arr[i]);
+    }
+    return 0;
+
+}
